fbb5cab582eac547841caffd6e63177ffb2f8400
james <james.barbosa@gmail.com>
2024-03-08 11:33:25 +0800

    Added event to create issue button

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { IssuesListComponent } from '../issues-list/issues-list.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [IssuesListComponent],
+  imports: [IssuesListComponent, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  
+  openCreateIssueDialog() {
 
+  }
 }
