0f1794f9972a91ce2253ce7499881265d69f5280
james <james.barbosa@gmail.com>
2024-03-08 11:55:25 +0800

    Added new endpoint to create issue

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+import fs from 'node:fs/promises';
+
 import bodyParser from 'body-parser';
 import express from 'express';
 
@@ -14,6 +16,13 @@ app.use((req, res, next) => {
     next();
 });
 
+app.post('/issue', async (req, res) => {
+    const fileContent = await fs.readFile('./data/success.json');
+
+    setTimeout(() => {
+        res.status(200).send(fileContent)
+    }, 10)
+});
 
 app.listen(3000, 'localhost', () => {
     console.log("Server running in port: ", 3000)
