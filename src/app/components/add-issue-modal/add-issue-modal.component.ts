891644c61c20dc096dd1fd95c3909b27421dad78
james <james.barbosa@gmail.com>
2024-03-08 11:56:01 +0800

    Added form on modal. Stylings.

--- src/app/components/add-issue-modal/add-issue-modal.component.ts
+++ src/app/components/add-issue-modal/add-issue-modal.component.ts
@@ -1,16 +1,31 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-issue-modal',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './add-issue-modal.component.html',
   styleUrl: './add-issue-modal.component.scss'
 })
-export class AddIssueModalComponent {
+export class AddIssueModalComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<AddIssueModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+    @Inject(MAT_DIALOG_DATA) public data: any, private _fb: FormBuilder) {}
 
-    
+  form: FormGroup | any;
+
+  ngOnInit(): void {
+    this.form = this._fb.group({
+      issueName: ['', Validators.required],
+    })
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  add() {
+    this.dialogRef.close(this.form.value)
+  }
 }
